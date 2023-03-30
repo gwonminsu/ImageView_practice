338f1ecffcc16cb8187074b0cd1f2d2da3826eb5
권민수 <kd0615@naver.com>
2023-03-30 11:27:09 +0900

    이미지뷰어 만들기 연습

--- ImageView_practice/ViewController.swift
+++ ImageView_practice/ViewController.swift
@@ -8,12 +8,42 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var indexCount = 0
+    var img: UIImage?
+    var imgString = ["cat.jpeg", "cat2.png", "catworm.jpeg", "devilcat.jpeg", "fanxycat.jpeg", "hmmchitcat.jpeg", "ironcat.jpeg", "nerdcat.jpeg", "sadcat.jpeg", "sadcat2.jpeg"]
+    
+    @IBOutlet var btnPrev: UIButton!
+    @IBOutlet var btnNext: UIButton!
+    @IBOutlet var ImgView: UIImageView!
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        img = UIImage(named: imgString[0])
+        
+        ImgView.image = img
     }
 
-
+    @IBAction func btnPrevAction(_ sender: UIButton) {
+        indexCount -= 1
+        if (indexCount < 0) {
+            indexCount = 0
+        }
+        img = UIImage(named: imgString[indexCount])
+        
+        ImgView.image = img
+    }
+    
+    
+    @IBAction func btnNextAction(_ sender: UIButton) {
+        indexCount += 1
+        if (indexCount > 9) {
+            indexCount = 9
+        }
+        img = UIImage(named: imgString[indexCount])
+        
+        ImgView.image = img
+    }
+    
 }
 
